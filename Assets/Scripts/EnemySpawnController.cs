2b25e089285597781e1b81f2763da4c7a948651c
Penguin <son@vdburg.site>
2026-05-18 10:54:34 +0200

    Enemy spawn interval issue

--- Assets/Scripts/EnemySpawnController.cs
+++ Assets/Scripts/EnemySpawnController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemySpawnController : MonoBehaviour
 {
@@ -6,11 +7,28 @@ public class EnemySpawnController : MonoBehaviour
     
     private float startDelay = 4.0f;
     private float interval = 5.0f;
+    private float minimumInterval = 1.5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnEnemy", startDelay, interval);
+        StartCoroutine(SpawnLoop());
+    }
+
+    // Spawn enemies at an decreasing interval
+    IEnumerator SpawnLoop()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        // Continuously spawn enemies
+        while (true)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(interval);
+            
+            // Decrease the interval if it's above the minimum interval
+            interval = Mathf.Max(minimumInterval, interval - 0.5f);
+        }
     }
 
     // Generate a random spawn position within the defined range
@@ -25,11 +43,5 @@ public class EnemySpawnController : MonoBehaviour
     void SpawnEnemy()
     {
         Instantiate(enemyVehicle, GenerateSpawnPosition(), enemyVehicle.transform.rotation);
-
-        // Only decrease spawn interval if it is greater than 0.5
-        if (interval > 0.5f)
-        {
-            interval -= 0.5f;
-        }
     }
 }
